3063f73ed3d4047c142cbac36a9668301f0925b5
Juan Aranda <Juan Aranda>
2024-08-03 13:46:27 -0500

    Se crean nuevos componentes base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AgrappProjectsListCardComponent } from './components/agrapp/agrapp-projects-list-card/agrapp-projects-list-card.component';
 import { LoginComponent } from './components/authentication/components/login/login.component';
 import { RegisterComponent } from './components/authentication/components/register/register.component';
+import { InputMultiRadioCheckComponent } from './core/components/input-multi-radio-check/input-multi-radio-check.component';
 import { BodyComponent } from './layout/components/body/body.component';
-import { AgrappProjectsComponent } from './pages/agrapp-projects/agrapp-projects.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'portal', pathMatch: 'full' },
   {
     path: 'portal', component: BodyComponent, children: [
-      { path: '', component: AgrappProjectsComponent },
+      { path: '', component: InputMultiRadioCheckComponent },
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent }
     ]
--- src/app/core/components/input-multi-radio-check/input-multi-radio-check.component.ts
+++ src/app/core/components/input-multi-radio-check/input-multi-radio-check.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
-  selector: 'app-input-multi-radio-check',
+  selector: 'input-multi-radio-check',
   templateUrl: './input-multi-radio-check.component.html',
-  styleUrl: './input-multi-radio-check.component.css'
+  styleUrl: './input-multi-radio-check.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputMultiRadioCheckComponent),
+      multi: true
+    }
+  ]
 })
-export class InputMultiRadioCheckComponent {
+export class InputMultiRadioCheckComponent implements ControlValueAccessor {
 
+  @Input() label: string = "";
+  @Input() placeholder: string = "";
+
+
+  value: boolean = false;
+  onChange: any = () => { };
+  onTouched: any = () => { };
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+  }
+
+  onInputChange(event: any) {
+    const value = event.target.value;
+    this.value = value === "true" ? true : false;
+    this.onChange(value);
+    this.onTouched();
+  }
 }
