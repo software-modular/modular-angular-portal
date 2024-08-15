cfcaf4dad3230083217c96a749b3b797ed0723dc
LUIS\luisa <luis.gomezr@carvajal.com>
2024-08-15 15:57:10 -0500

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AgrappProjectsRegisterComponent } from './components/agrapp/agrapp-projects-register/agrapp-projects-register.component';
 import { LoginComponent } from './core/pages/login/login.component';
 import { RegisterComponent } from './core/pages/register/register.component';
 import { UserProfileComponent } from './core/pages/user-profile/user-profile.component';
 import { BodyComponent } from './layout/components/body/body.component';
 import { authenticationGuard } from './core/guards/authentication.guard';
 import { AgrappManageUserComponent } from './pages/agrapp-manage-user/agrapp-manage-user.component';
+import { AgrappHomeComponent } from './pages/agrapp-home/agrapp-home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'portal', pathMatch: 'full' },
@@ -16,7 +16,7 @@ const routes: Routes = [
       { path: 'register', component: RegisterComponent },
       { path: 'user/profile', component: UserProfileComponent, canActivate: [authenticationGuard()] },
       { path: 'user/management', component: AgrappManageUserComponent, canActivate: [authenticationGuard()] },
-      { path: 'home', component: AgrappProjectsRegisterComponent, canActivate: [authenticationGuard()] },
+      { path: 'home', component: AgrappHomeComponent },
     ]
   }
 
