880153a140096f8821f4c28be2d55c0200662e2c
Juan Aranda <Juan Aranda>
2024-09-12 21:39:17 -0500

    Se hacen correciones css

--- src/app/pages/agrapp-home/agrapp-home.component.ts
+++ src/app/pages/agrapp-home/agrapp-home.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { ProjectService } from '../../core/services/project/project.service';
 import { ProjectDto } from '../../core/domain/dto/projectDto';
-import { getCityByCode, getDepartmentByCode } from '../../core/domain/const/Colombia';
+import {
+  getCityByCode,
+  getDepartmentByCode,
+} from '../../core/domain/const/Colombia';
 import { AgrappCardInput } from '../../core/domain/beans/agrappCardInput';
 import { projectList } from '../../core/domain/const/Mocks';
 import { AuthenticationService } from '../../core/services/authentication/authentication.service';
@@ -10,14 +13,15 @@ import { environment } from '../../../environments/environment';
 @Component({
   selector: 'agrapp-home',
   templateUrl: './agrapp-home.component.html',
-  styleUrl: './agrapp-home.component.css'
+  styleUrl: './agrapp-home.component.css',
 })
 export class AgrappHomeComponent {
+  projects: ProjectDto[] = [];
 
-  constructor(private projectService: ProjectService,
-    private authenticationService: AuthenticationService,
-  ) {
-  }
+  constructor(
+    private projectService: ProjectService,
+    private authenticationService: AuthenticationService
+  ) {}
 
   userIsAuthenticate(): Boolean {
     return this.authenticationService.userIsAuthenticated();
@@ -25,5 +29,16 @@ export class AgrappHomeComponent {
 
   getWhatsappNumber() {
     return environment.aditionalInfo.whatsappNumber;
+  }
+
+  getProjects() {
+    this.projectService.findAllPublicProjects().subscribe({
+      next: (data) => {
+        this.projects = data.results;
+      },
+      error: (_) => {
+        console.log('No se encontraron proyectos');
+      },
+    });
   }
 }
